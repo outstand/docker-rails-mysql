a7d5f1c04a0246dfa0012ba5998377fc23465b35
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-03-01 14:44:26 -0800

    Upgrade init hook

--- test_database.sh
+++ test_database.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -e -x
+set -e
 
-if [ "$MYSQL_DATABASE" ]; then
-  echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}_test\` ;" | "${mysql[@]}"
+if [ -n "$MYSQL_DATABASE" ]; then
+  mysql_note "Creating database ${MYSQL_DATABASE}_test"
+  docker_process_sql --database=mysql <<<"CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}_test\` ;"
 
-  if [ "$MYSQL_USER" ]; then
-    echo "GRANT ALL ON \`${MYSQL_DATABASE}_test\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
-    echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+  if [ -n "$MYSQL_USER" ]; then
+    mysql_note "Giving user ${MYSQL_USER} access to schema ${MYSQL_DATABASE}_test"
+    docker_process_sql --database=mysql <<<"GRANT ALL ON \`${MYSQL_DATABASE}_test\`.* TO '$MYSQL_USER'@'%' ;"
+    docker_process_sql --database=mysql <<<"FLUSH PRIVILEGES ;"
   fi
 fi

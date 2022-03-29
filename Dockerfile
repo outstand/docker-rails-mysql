d38e1b70d0efd9f1c16a59173db7654e0580f526
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-03-29 20:23:25 +0000

    Create multiple databases

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ LABEL maintainer="Ryan Schlesinger <ryan@outstand.com>"
 
 COPY test_database.sh /docker-entrypoint-initdb.d/test_database.sh
 COPY charset.cnf /etc/mysql/conf.d/charset.cnf
+COPY docker-entrypoint.sh /usr/local/bin/
--- test_database.sh
+++ test_database.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-set -e
 
-if [ -n "$MYSQL_DATABASE" ]; then
-  mysql_note "Creating database ${MYSQL_DATABASE}_test"
-  docker_process_sql --database=mysql <<<"CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}_test\` ;"
+set -eo pipefail
 
-  if [ -n "$MYSQL_USER" ]; then
-    mysql_note "Giving user ${MYSQL_USER} access to schema ${MYSQL_DATABASE}_test"
-    docker_process_sql --database=mysql <<<"GRANT ALL ON \`${MYSQL_DATABASE}_test\`.* TO '$MYSQL_USER'@'%' ;"
+create_database() {
+  name="$1"
+
+  mysql_note "Creating database ${name}"
+  docker_process_sql --database=mysql <<<"CREATE DATABASE IF NOT EXISTS \`${name}\` ;"
+
+  if [ -n "${MYSQL_USER:-}" ]; then
+    mysql_note "Giving user ${MYSQL_USER} access to schema ${name}"
+    docker_process_sql --database=mysql <<<"GRANT ALL ON \`${name}\`.* TO '${MYSQL_USER}'@'%' ;"
     docker_process_sql --database=mysql <<<"FLUSH PRIVILEGES ;"
   fi
+}
+
+if [ -n "${MYSQL_DATABASES:-}" ]; then
+  IFS=',' read -ra databases <<< "$MYSQL_DATABASES"
+
+  for name in "${databases[@]}"; do
+    create_database "${name}"
+    create_database "${name}_test"
+  done
 fi
